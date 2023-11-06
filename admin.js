3a098abe0fb0fa51b7d6453a032723e27c134418
Laura902l <121919112+Laura902l@users.noreply.github.com>
2023-11-06 10:55:42 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get all user data from Local Storage
+
     const allUserData = JSON.parse(localStorage.getItem('userAccounts')) || [];
   
-    // Get the user list container
+
     const userList = document.getElementById('userList');
   
-    // Display each user account as a separate block
+
     allUserData.forEach(function (userData, index) {
       const userBlock = document.createElement('div');
       userBlock.className = 'user-block';
@@ -20,17 +20,16 @@ document.addEventListener('DOMContentLoaded', function () {
       userList.appendChild(userBlock);
     });
   
-    // Add a click event listener to each "Delete User" button
+
     const deleteButtons = document.querySelectorAll('.delete-user-button');
     deleteButtons.forEach(function (button) {
       button.addEventListener('click', function () {
         const index = button.getAttribute('data-index');
         if (confirm('Are you sure you want to delete this user?')) {
-          // Remove the user data from the array
+
           allUserData.splice(index, 1);
-          // Update Local Storage with the modified user data
+
           localStorage.setItem('userAccounts', JSON.stringify(allUserData));
-          // Remove the user block from the page
           userList.removeChild(button.parentElement);
           alert('User data deleted.');
         }
