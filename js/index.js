3798e8204a70f0ecc1dabdf7ec0fc8382358640b
Laura902l <121919112+Laura902l@users.noreply.github.com>
2023-11-24 13:37:40 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,9 +8,367 @@ var splide = new Splide('.splide', {
 splide.mount();
 
 
+//like
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".like-button1");
+
+  // Function to update the like buttons based on the user's login status
+  function updateLikeButtons() {
+    let userEmail = localStorage.getItem("userEmail");
+
+    buttons.forEach((button) => {
+      let userLikeKey = userEmail + "-" + button.id;
+      let isLiked = localStorage.getItem(userLikeKey) === "true";
+
+      if (isLiked) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
+
+  // Event listener for like buttons
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var isLoggedIn = localStorage.getItem("isLoggedIn");
+      var userEmail = localStorage.getItem("userEmail");
+
+      if (isLoggedIn === "true" && userEmail) {
+        let userLikeKey = userEmail + "-" + button.id;
+        let hasLiked = localStorage.getItem(userLikeKey) === "true";
+
+        if (!hasLiked) {
+          this.classList.toggle("active");
+          this.classList.add("animated");
+          generateClones(this);
+          localStorage.setItem(userLikeKey, this.classList.contains("active"));
+        } else {
+          this.classList.remove("active");
+          localStorage.removeItem(userLikeKey);
+        }
+      } else {
+        alert("Please log in or sign up.");
+      }
+    });
+  });
+
+  // Initial update of like buttons when the page loads
+  updateLikeButtons();
+
+  function generateClones(button) {
+    let clones = randomInt(2, 4);
+    for (let it = 1; it <= clones; it++) {
+      let clone = button.querySelector("svg").cloneNode(true),
+        size = randomInt(5, 16);
+      button.appendChild(clone);
+      clone.setAttribute("width", size);
+      clone.setAttribute("height", size);
+      clone.style.position = "absolute";
+      clone.style.transition =
+        "transform 0.5s cubic-bezier(0.12, 0.74, 0.58, 0.99) 0.3s, opacity 1s ease-out .5s";
+      let animTimeout = setTimeout(function () {
+        clearTimeout(animTimeout);
+        clone.style.transform =
+          "translate3d(" +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px," +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px,0)";
+        clone.style.opacity = 0;
+      }, 1);
+      let removeNodeTimeout = setTimeout(function () {
+        clone.parentNode.removeChild(clone);
+        clearTimeout(removeNodeTimeout);
+      }, 900);
+      let removeClassTimeout = setTimeout(function () {
+        button.classList.remove("animated");
+      }, 600);
+    }
+  }
+
+  function plusOrMinus() {
+    return Math.random() < 0.5 ? -1 : 1;
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+});
+// like 2
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".like-button2");
+
+  // Function to update the like buttons based on the user's login status
+  function updateLikeButtons() {
+    let userEmail = localStorage.getItem("userEmail");
+
+    buttons.forEach((button) => {
+      let userLikeKey = userEmail + "-" + button.id;
+      let isLiked = localStorage.getItem(userLikeKey) === "true";
+
+      if (isLiked) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
+
+  // Event listener for like buttons
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var isLoggedIn = localStorage.getItem("isLoggedIn");
+      var userEmail = localStorage.getItem("userEmail");
+
+      if (isLoggedIn === "true" && userEmail) {
+        let userLikeKey = userEmail + "-" + button.id;
+        let hasLiked = localStorage.getItem(userLikeKey) === "true";
+
+        if (!hasLiked) {
+          this.classList.toggle("active");
+          this.classList.add("animated");
+          generateClones(this);
+          localStorage.setItem(userLikeKey, this.classList.contains("active"));
+        } else {
+          this.classList.remove("active");
+          localStorage.removeItem(userLikeKey);
+        }
+      } else {
+        alert("Please log in or sign up.");
+      }
+    });
+  });
+
+  // Initial update of like buttons when the page loads
+  updateLikeButtons();
+
+  function generateClones(button) {
+    let clones = randomInt(2, 4);
+    for (let it = 1; it <= clones; it++) {
+      let clone = button.querySelector("svg").cloneNode(true),
+        size = randomInt(5, 16);
+      button.appendChild(clone);
+      clone.setAttribute("width", size);
+      clone.setAttribute("height", size);
+      clone.style.position = "absolute";
+      clone.style.transition =
+        "transform 0.5s cubic-bezier(0.12, 0.74, 0.58, 0.99) 0.3s, opacity 1s ease-out .5s";
+      let animTimeout = setTimeout(function () {
+        clearTimeout(animTimeout);
+        clone.style.transform =
+          "translate3d(" +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px," +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px,0)";
+        clone.style.opacity = 0;
+      }, 1);
+      let removeNodeTimeout = setTimeout(function () {
+        clone.parentNode.removeChild(clone);
+        clearTimeout(removeNodeTimeout);
+      }, 900);
+      let removeClassTimeout = setTimeout(function () {
+        button.classList.remove("animated");
+      }, 600);
+    }
+  }
+
+  function plusOrMinus() {
+    return Math.random() < 0.5 ? -1 : 1;
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+});
+//like 3
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".like-button3");
+
+  // Function to update the like buttons based on the user's login status
+  function updateLikeButtons() {
+    let userEmail = localStorage.getItem("userEmail");
+
+    buttons.forEach((button) => {
+      let userLikeKey = userEmail + "-" + button.id;
+      let isLiked = localStorage.getItem(userLikeKey) === "true";
+
+      if (isLiked) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
+
+  // Event listener for like buttons
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var isLoggedIn = localStorage.getItem("isLoggedIn");
+      var userEmail = localStorage.getItem("userEmail");
+
+      if (isLoggedIn === "true" && userEmail) {
+        let userLikeKey = userEmail + "-" + button.id;
+        let hasLiked = localStorage.getItem(userLikeKey) === "true";
+
+        if (!hasLiked) {
+          this.classList.toggle("active");
+          this.classList.add("animated");
+          generateClones(this);
+          localStorage.setItem(userLikeKey, this.classList.contains("active"));
+        } else {
+          this.classList.remove("active");
+          localStorage.removeItem(userLikeKey);
+        }
+      } else {
+        alert("Please log in or sign up.");
+      }
+    });
+  });
+
+  // Initial update of like buttons when the page loads
+  updateLikeButtons();
+
+  function generateClones(button) {
+    let clones = randomInt(2, 4);
+    for (let it = 1; it <= clones; it++) {
+      let clone = button.querySelector("svg").cloneNode(true),
+        size = randomInt(5, 16);
+      button.appendChild(clone);
+      clone.setAttribute("width", size);
+      clone.setAttribute("height", size);
+      clone.style.position = "absolute";
+      clone.style.transition =
+        "transform 0.5s cubic-bezier(0.12, 0.74, 0.58, 0.99) 0.3s, opacity 1s ease-out .5s";
+      let animTimeout = setTimeout(function () {
+        clearTimeout(animTimeout);
+        clone.style.transform =
+          "translate3d(" +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px," +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px,0)";
+        clone.style.opacity = 0;
+      }, 1);
+      let removeNodeTimeout = setTimeout(function () {
+        clone.parentNode.removeChild(clone);
+        clearTimeout(removeNodeTimeout);
+      }, 900);
+      let removeClassTimeout = setTimeout(function () {
+        button.classList.remove("animated");
+      }, 600);
+    }
+  }
+
+  function plusOrMinus() {
+    return Math.random() < 0.5 ? -1 : 1;
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+});
 
 
 
+//like
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".like-button1");
+
+  // Function to update the like buttons based on the user's login status
+  function updateLikeButtons() {
+    let userEmail = localStorage.getItem("userEmail");
+
+    buttons.forEach((button) => {
+      let userLikeKey = userEmail + "-" + button.id;
+      let isLiked = localStorage.getItem(userLikeKey) === "true";
+
+      if (isLiked) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  }
+
+  // Event listener for like buttons
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var isLoggedIn = localStorage.getItem("isLoggedIn");
+      var userEmail = localStorage.getItem("userEmail");
+
+      if (isLoggedIn === "true" && userEmail) {
+        let userLikeKey = userEmail + "-" + button.id;
+        let hasLiked = localStorage.getItem(userLikeKey) === "true";
+
+        if (!hasLiked) {
+          this.classList.toggle("active");
+          this.classList.add("animated");
+          generateClones(this);
+          localStorage.setItem(userLikeKey, this.classList.contains("active"));
+        } else {
+          this.classList.remove("active");
+          localStorage.removeItem(userLikeKey);
+        }
+      } else {
+        alert("Please log in or sign up.");
+      }
+    });
+  });
+
+  // Initial update of like buttons when the page loads
+  updateLikeButtons();
+
+  function generateClones(button) {
+    let clones = randomInt(2, 4);
+    for (let it = 1; it <= clones; it++) {
+      let clone = button.querySelector("svg").cloneNode(true),
+        size = randomInt(5, 16);
+      button.appendChild(clone);
+      clone.setAttribute("width", size);
+      clone.setAttribute("height", size);
+      clone.style.position = "absolute";
+      clone.style.transition =
+        "transform 0.5s cubic-bezier(0.12, 0.74, 0.58, 0.99) 0.3s, opacity 1s ease-out .5s";
+      let animTimeout = setTimeout(function () {
+        clearTimeout(animTimeout);
+        clone.style.transform =
+          "translate3d(" +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px," +
+          (plusOrMinus() * randomInt(10, 25)) +
+          "px,0)";
+        clone.style.opacity = 0;
+      }, 1);
+      let removeNodeTimeout = setTimeout(function () {
+        clone.parentNode.removeChild(clone);
+        clearTimeout(removeNodeTimeout);
+      }, 900);
+      let removeClassTimeout = setTimeout(function () {
+        button.classList.remove("animated");
+      }, 600);
+    }
+  }
+
+  function plusOrMinus() {
+    return Math.random() < 0.5 ? -1 : 1;
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+});
 function findRecipe() {
 
   let favDrink = prompt("Please select the recipe category: \n\n1 -    CUPCAKE \n2 -   BREAKFAST \n3 -   CAKE");
@@ -158,14 +516,6 @@ function goToProfile() {
 
 
 
-
-
-
-
-
-
-
-
 // function startTimer(duration) {
 //   const timerElement = document.getElementById("timer");
 //   timerElement.textContent = "Timer: " + (duration / 1000);
@@ -209,5 +559,16 @@ function goToProfile() {
 
 
 
+function getLocalStorageItem(key) {
+  return localStorage.getItem(key);
+}
 
+function setLocalStorageItem(key, value) {
+  localStorage.setItem(key, value);
+}
 
+function getUserEmail() {
+  // Вместо использования жестко закодированной почты, здесь можно добавить свой механизм получения почты пользователя из локального хранилища
+  // Например, если вы храните почту под ключом "userEmail":
+  return getLocalStorageItem('userEmail') || 'example@example.com';
+}
